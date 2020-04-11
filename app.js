877cd2f0f77004ef2350e60f14046c4c183fbf6d
Joao Juliatti <joaopjuliatti@gmail.com>
2020-04-11 10:28:10 -0300

    Trazendo os updates do Paulo para branch master

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/project', {useNewUrlParser: true})
+  .connect('mongodb://localhost/project2', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
